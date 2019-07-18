6359be1a25e8f950f807aa94f1760f615fbb8cd9
Andrew Chow <andrewcxw@gmail.com>
2019-07-18 22:52:37 +0100

    Refactoring project structure

--- MultipleObjectTracking.py
+++ MultipleObjectTracking.py
@@ -7,9 +7,9 @@ def main():
     data = mot.parse("20190716T195132.csv")
     state = State.from_ob(data.pop(0))
     p_state = state.predict_at(data[0].time)
-    p_state.distance(data[0])
-    p_state.distance(data[1])
-    p_state.distance(data[2])
+    pprint(p_state.distance(data[0]))
+    pprint(p_state.distance(data[1]))
+    pprint(p_state.distance(data[2]))
 
 
 if __name__ == "__main__":
